1207aecf9e31709430da014a8d1e2020ae460a83
Clinton Wong <clint.isaac.wong@gmail.com>
2013-10-28 18:32:03 -0600

    Uploaded starter skeleton code

---
+++ parallel_merge_sort.rb
@@ -0,0 +1,40 @@
+require 'test/unit/assertions.rb'
+include Test::Unit::Assertions
+
+# Module that implements multi-threaded merge sort
+module ParallelMergeSort
+	
+
+	def self.MergeSort(duration, a)
+		# PRE Conditions
+		begin
+			raise ArgumentError, "ParallelMergeSort::ArgumentError -> duration is not a positive number" unless duration > 0
+		end
+		
+		begin
+			raise ArgumentError, "ParallelMergeSort::ArgumentError -> a is not an array." unless a.kind_of? Array
+		end
+
+		begin
+			raise ArgumentError, "ParallelMergeSort::ArgumentError -> a is empty." unless (!a.empty?)
+		end
+
+		begin
+			a.each {|element|
+				raise ArgumentError, "ParallelMergeSort::ArgumentError -> Elements do not implement Comparable." unless a.first.included_modules.include? Comparable
+			}
+		end
+		# End PRE Conditions
+
+		@timeTaken = 0
+
+		# POST Conditions
+		assert(@timeTaken < duration)
+		# End POST Conditions
+	end
+
+	def invariant
+
+	end
+
+end
