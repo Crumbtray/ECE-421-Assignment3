9f2c82f2e112763732dd595f8ca73fb73e716900
Clinton Wong <clint.isaac.wong@gmail.com>
2013-11-12 13:31:52 -0700

    Fleshed out PMerge

--- parallel2.rb
+++ parallel2.rb
@@ -31,6 +31,23 @@ module ParallelMergeSort
 	end
 
 	def self.PMerge(a, b)
+		result = Array.new
 		puts "PMerge with #{a.to_s} and #{b.to_s}"
+		# The idea here is to concatenate a with b, but finding the right place to merge by using binary search.
+		if(b.empty?)
+			return a
+		elsif(a.empty?)
+			return b
+		elsif(a.length == 1 and b.length == 1)
+			if (a.first <= b.first)
+				return result.push(a).push(b)
+			else
+				return result.push(b).push(a)
+			end
+		else
+			# Split A and B by finding J through binary search.
+			# Crude filler right now.
+			return a.concat(b)
+		end
 	end
 end
