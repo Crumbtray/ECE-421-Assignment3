f1fc7f0acda25ee3dca68c6987bdf9991566a3b9
jmorawsk <jmorawsk@ualberta.ca>
2013-11-11 23:16:44 -0700

    Added some tests with debug puts

--- merge_sort_tests.rb
+++ merge_sort_tests.rb
@@ -14,9 +14,52 @@ class MergeSortTests < Test::Unit::TestCase
 			ParallelMergeSort.MergeSort(10, a)
 		end
   	end
-
-  	def test_basic_numbers
-  		a = [1, 2, 3, 4]
+	
+	def test_two_sorted_numbers
+  		a = [1, 2]
   		ParallelMergeSort.MergeSort(10, a)
+		b = [1, 2]
+		puts 'arrays'
+		puts a
+		puts b
+		assert_equal(a,b)
+  	end
+	
+	def test_two_reverse_numbers
+  		a = [2, 1]
+  		ParallelMergeSort.MergeSort(10, a)
+		b = [1, 2]
+		puts 'arrays'
+		puts a
+		puts b
+		assert_equal(a,b)
+  	end
+
+  	def test_basic_sorted_numbers
+  		a = [1, 2, 3]
+  		ParallelMergeSort.MergeSort(10, a)
+		b = [1, 2, 3]
+		assert_equal(a,b)
+  	end
+	
+	def test_basic_unsorted_numbers
+  		a = [3, 2, 1]
+  		ParallelMergeSort.MergeSort(10, a)
+		b = [2, 3, 1]
+		assert_equal(a,b)
+  	end
+	
+	def atest_many_sorted_numbers
+  		a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  		ParallelMergeSort.MergeSort(10, a)
+		b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+		assert_equal(a,b)
+  	end
+	
+	def atest_many_unsorted_numbers
+  		a = [5, 7, 9, 0, 2, 4, 6, 10, 8, 3, 1]
+  		ParallelMergeSort.MergeSort(10, a)
+		b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+		assert_equal(a,b)
   	end
 end
