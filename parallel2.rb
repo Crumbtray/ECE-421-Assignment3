48002e4db56ba0e19f89551fe2f3b0d1b05a96c6
Clinton Wong <clint.isaac.wong@gmail.com>
2013-11-12 11:49:11 -0700

    More fleshing of Parallel2

--- parallel2.rb
+++ parallel2.rb
@@ -2,9 +2,17 @@ module ParallelMergeSort
 	def self.MergeSort(a)
 		if a.length == 1
 			return a
-
-
-
+		else
+			midpoint = a.length / 2
+			t1 = Thread.new do
+				self.MergeSort(a.take(midpoint + 1))
+			end
+			t2 = Thread.new do
+				self.MergeSort(a.drop(midpoint + 1))
+			end
+			t1.join
+			t2.join
+			self.PMerge(t1.value, t2.value)
+		end
 	end
-
 end
