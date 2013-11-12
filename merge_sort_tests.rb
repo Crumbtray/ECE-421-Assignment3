0145ace3b5dd4d4b1f830fb70c7782ea1a37da4c
jmorawsk <jmorawsk@ualberta.ca>
2013-11-12 00:04:01 -0700

    Fixed tests to use return value

--- merge_sort_tests.rb
+++ merge_sort_tests.rb
@@ -17,7 +17,7 @@ class MergeSortTests < Test::Unit::TestCase
 	
 	def test_two_sorted_numbers
   		a = [1, 2]
-  		ParallelMergeSort.MergeSort(10, a)
+  		a = ParallelMergeSort.MergeSort(10, a)
 		b = [1, 2]
 		puts 'arrays'
 		puts a
@@ -27,7 +27,7 @@ class MergeSortTests < Test::Unit::TestCase
 	
 	def test_two_reverse_numbers
   		a = [2, 1]
-  		ParallelMergeSort.MergeSort(10, a)
+  		a = ParallelMergeSort.MergeSort(10, a)
 		b = [1, 2]
 		puts 'arrays'
 		puts a
@@ -37,28 +37,28 @@ class MergeSortTests < Test::Unit::TestCase
 
   	def test_basic_sorted_numbers
   		a = [1, 2, 3]
-  		ParallelMergeSort.MergeSort(10, a)
+  		a = ParallelMergeSort.MergeSort(10, a)
 		b = [1, 2, 3]
 		assert_equal(a,b)
   	end
 	
 	def test_basic_unsorted_numbers
   		a = [3, 2, 1]
-  		ParallelMergeSort.MergeSort(10, a)
+  		a = ParallelMergeSort.MergeSort(10, a)
 		b = [2, 3, 1]
 		assert_equal(a,b)
   	end
 	
 	def atest_many_sorted_numbers
   		a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  		ParallelMergeSort.MergeSort(10, a)
+  		a = ParallelMergeSort.MergeSort(10, a)
 		b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 		assert_equal(a,b)
   	end
 	
 	def atest_many_unsorted_numbers
   		a = [5, 7, 9, 0, 2, 4, 6, 10, 8, 3, 1]
-  		ParallelMergeSort.MergeSort(10, a)
+  		a = ParallelMergeSort.MergeSort(10, a)
 		b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 		assert_equal(a,b)
   	end
